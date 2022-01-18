86ad5607c6bc53552e2827da4b4d8041646c3c92
Jekuru <FJOAndreu@hotmail.es>
2022-01-18 14:24:25 +0100

    Comenzando CharacterController
    Finalizado movimiento horizontal. Problemas con el salto.

---
+++ Assets/_Scripts/Controllers/Player2DController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Player2DController", menuName = "InputController/Player2DController")]
+public class Player2DController : InputController
+{
+    public override bool RetrieveJumpInput()
+    {
+        return Input.GetButtonDown("Jump");
+    }
+
+    public override float RetrieveMoveInput()
+    {
+        return Input.GetAxisRaw("Horizontal");
+    }
+}
