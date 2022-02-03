ee87229f4ed2d8aae5129b16defd431bf582bfea
Jekuru <FJOAndreu@hotmail.es>
2022-02-03 10:55:44 +0100

    Cambiado el comportamiento de los proyectiles físicos

--- Assets/_Scripts/Weapons/Bullet.cs
+++ Assets/_Scripts/Weapons/Bullet.cs
@@ -23,23 +23,54 @@ public class Bullet : MonoBehaviour
         {
             Destroy(gameObject);
         }
+
+        
     }
+
+    private void FixedUpdate()
+    {
+        Detection();
+    }
+
+
     /**
      * Nota:
      * Reducida la velocidad de las balas a 40 porque atraviesan los objetos.
      * Revisar colisión de la bala con el jugador, ya que no parece colisionar 2DCollider, igual es percepcion propia debido al trail y sprite oculto de la bala.
      * 
-     */
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.name != "FirePoint" && collision.name != "ProtoBullet(Clone)")
         {
-            Debug.Log(collision.name);
+            //Debug.Log(collision.name);
             if (collision.CompareTag("Player"))
             {
                 collision.GetComponent<PlayerStats>().healthPoints--;
             }
             Destroy(gameObject);
+        }
+    }     */
+
+    private void Detection()
+    {
+        LayerMask layers = LayerMask.GetMask("Player");
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, rBody.velocity, rBody.velocity.magnitude * Time.deltaTime, layers);
+        
+        if (hit.collider != null)
+        {
+            Debug.Log("Impact " + hit.collider.name);
+            Debug.DrawRay(transform.position, rBody.velocity, Color.green);
+            if (hit.collider.CompareTag("Player"))
+            {
+                hit.collider.GetComponent<PlayerStats>().healthPoints--;
+            }
+            Destroy(gameObject);
+        } else
+        {
+            Debug.Log("Not hit");
+            Debug.DrawRay(transform.position, rBody.velocity, Color.red);
         }
     }
 }
