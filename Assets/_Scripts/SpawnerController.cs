f2b58c9a5de1a0bc68183b8564a8f030296d6061
CarlosLopezCEV <carlos_lopez_tsapp1ma2021@cev.com>
2022-02-10 11:57:08 +0100

    weapon spawner finished

--- Assets/_Scripts/SpawnerController.cs
+++ Assets/_Scripts/SpawnerController.cs
@@ -8,7 +8,11 @@ public class SpawnerController : MonoBehaviour
     public float spawnTime;
     private float timer = 0.0f;
     public string spawnedWeapon;
+    public string[] avaliableWeapons;
     public bool isActive = true;
+    int index;
+    public Sprite[] spriteList;
+
 
 
     // Update is called once per frame
@@ -20,11 +24,70 @@ public class SpawnerController : MonoBehaviour
             timer += Time.deltaTime;
             if (timer > spawnTime)
             {
-                weaponSpawner.SetActive(false);
+                weaponSpawner.SetActive(true);
+                gameObject.GetComponent<BoxCollider2D>().enabled = true;
                 isActive = true;
+
+                index = Random.Range(0, avaliableWeapons.Length);
+                spawnedWeapon = avaliableWeapons[index];
             }
 
         }
+
+        switch (spawnedWeapon)
+        {
+            case "sword":
+                weaponSpawner.GetComponent<SpriteRenderer>().sprite = spriteList[0];
+                break;
+
+            case "dagger":
+                weaponSpawner.GetComponent<SpriteRenderer>().sprite = spriteList[1];
+                break;
+
+            case "greatsword":
+                weaponSpawner.GetComponent<SpriteRenderer>().sprite = spriteList[2];
+                break;
+
+            case "halberd":
+                weaponSpawner.GetComponent<SpriteRenderer>().sprite = spriteList[3];
+                break;
+
+            case "pistol":
+                weaponSpawner.GetComponent<SpriteRenderer>().sprite = spriteList[4];
+                break;
+
+            case "shotgun":
+                weaponSpawner.GetComponent<SpriteRenderer>().sprite = spriteList[5];
+                break;
+
+            case "rifle":
+                weaponSpawner.GetComponent<SpriteRenderer>().sprite = spriteList[6];
+                break;
+
+            case "sniper":
+                weaponSpawner.GetComponent<SpriteRenderer>().sprite = spriteList[7];
+                break;
+
+            case "rpg":
+                weaponSpawner.GetComponent<SpriteRenderer>().sprite = spriteList[8];
+                break;
+
+            case "raygun":
+                weaponSpawner.GetComponent<SpriteRenderer>().sprite = spriteList[9];
+                break;
+
+            case "grenade":
+                weaponSpawner.GetComponent<SpriteRenderer>().sprite = spriteList[10];
+                break;
+
+
+
+
+        }
+
+
+
+
     }
 
     void OnCollisionEnter2D(Collision2D col)
@@ -32,7 +95,9 @@ public class SpawnerController : MonoBehaviour
         if (col.collider.gameObject.CompareTag("Player"))
         {
             weaponSpawner.SetActive(false);
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
             isActive = false;
+            timer = 0f;
         }
     }
 
