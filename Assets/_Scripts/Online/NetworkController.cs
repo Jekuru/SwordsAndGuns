9235e20eaa65d2bf90a49fc5216aade53877961f
Jekuru <FJOAndreu@hotmail.es>
2022-03-07 14:36:58 +0100

    Combinando funcionalidad online con interfaz para crear partida

---
+++ Assets/_Scripts/Online/NetworkController.cs
@@ -0,0 +1,176 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+using UnityEngine.UI;
+
+public class NetworkController : MonoBehaviourPunCallbacks
+{
+    // *** Asignar desde el inspector ***
+    [SerializeField] private PlayerPreferences playerPreferences;
+
+    // *** UI *** //
+    [SerializeField] private TMP_InputField roomNumberInput;
+    [SerializeField] private TMP_Text roomNumber;
+    [SerializeField] private Button commenceButton;
+
+    // *** Fin UI *** ///
+
+    // *** Fin Inspector *** //
+
+    [SerializeField] private int randomRoomNumber;
+
+    [SerializeField] private Transform playersContainer;
+    [SerializeField] private GameObject playerListingPrefab;
+
+    private void Update()
+    {
+        if (PhotonNetwork.InRoom)
+        {
+            Debug.Log("In room");
+            Debug.Log("Jugadores en la sala: " + PhotonNetwork.PlayerList.Length);
+            Debug.Log(PhotonNetwork.MasterClient.NickName); 
+
+            if (PhotonNetwork.CurrentRoom.PlayerCount >= 1) // TODO: 1 por testeo, 2 es el valor que debería tener...
+            {
+                commenceButton.interactable = true;
+            }
+            else
+            {
+                commenceButton.interactable = false;
+            }
+        }
+    }
+
+    #region Botones
+    public void ButtonQuickPlay()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public void ButtonBack()
+    {
+        PhotonNetwork.LeaveRoom();
+        PhotonNetwork.LeaveLobby();
+        StartCoroutine(rejoinLobby());
+    }
+
+    public void ButtonDisconnect()
+    {
+        OnLeftRoom();
+    }
+
+    public void ButtonCommence()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.CurrentRoom.IsOpen = false;
+            StartCoroutine(LoadScene());
+        }
+    }
+    #endregion
+
+    IEnumerator LoadScene()
+    {
+        yield return new WaitForSeconds(2);
+        Debug.Log("Cargando escena...");
+        PhotonNetwork.LoadLevel("Map2");
+    }
+
+    IEnumerator rejoinLobby()
+    {
+        yield return new WaitForSeconds(1);
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Conectado al servidor " + PhotonNetwork.CloudRegion);
+
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.NickName = playerPreferences.nickName;
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnLeftRoom()
+    {
+        PhotonNetwork.Disconnect();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        if(message == "No match found")
+        {
+            Debug.Log("No se ha encontrado una sala libre.");
+        }
+        else
+        {
+            Debug.LogError("Error: " + message);
+        }
+        
+        Debug.Log("Creando nueva sala...");
+        RoomOptions r = new RoomOptions
+        {
+            MaxPlayers = 4,
+            IsVisible = true,
+            IsOpen = true
+        };
+        randomRoomNumber = Random.Range(100000, 999999);
+        PhotonNetwork.JoinOrCreateRoom(randomRoomNumber.ToString(), r, TypedLobby.Default);
+        roomNumber.text = randomRoomNumber.ToString();
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("No se pudo crear una nueva sala.\n" + returnCode + " - " + message);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Uniéndose como: " + PhotonNetwork.LocalPlayer.NickName);
+
+        roomNumber.text = PhotonNetwork.CurrentRoom.Name;
+        commenceButton.gameObject.SetActive(PhotonNetwork.IsMasterClient);
+
+        ClearPlayerListings();
+        ListPlayers();
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log(newPlayer.NickName + " se ha unido a la sala.");
+        ClearPlayerListings();
+        ListPlayers();
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        Debug.Log(otherPlayer.NickName + " se ha ido de la sala.");
+
+        ClearPlayerListings();
+        ListPlayers();
+        if (PhotonNetwork.IsMasterClient)
+            commenceButton.gameObject.SetActive(true);
+
+    }
+
+    void ClearPlayerListings()
+    {
+        for (int i = playersContainer.childCount - 1; i >= 0; i--)
+        {
+            Destroy(playersContainer.GetChild(i).gameObject);
+        }
+    }
+
+    void ListPlayers()
+    {
+        foreach (Player player in PhotonNetwork.PlayerList)
+        {
+            GameObject tempListing = Instantiate(playerListingPrefab, playersContainer);
+            TMP_Text tempText = tempListing.transform.GetChild(0).GetComponent<TMP_Text>();
+            tempText.text = player.NickName;
+        }
+    }
+}
