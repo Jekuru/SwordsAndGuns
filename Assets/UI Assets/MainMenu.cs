0f5c22be104adca69cd8e48ed8a2ec8027e61e72
CarlosLopezCEV <carlos_lopez_tsapp1ma2021@cev.com>
2022-03-09 14:20:57 +0100

    menu opciones

--- Assets/UI Assets/MainMenu.cs
+++ Assets/UI Assets/MainMenu.cs
@@ -2,9 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public TMPro.TMP_Dropdown resolutionDrop;
+    Resolution[] resolutions;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDrop.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDrop.AddOptions(options);
+        resolutionDrop.value = currentResolutionIndex;
+        resolutionDrop.RefreshShownValue();
+
+    }
+
+    public void setResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void setFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+
+    public void setGeneralVolume (float generalVolume)
+    {
+
+    }
+
+
     public void playButton()
     {
         
